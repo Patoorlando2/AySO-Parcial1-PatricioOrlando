6cbf48742f02616a95ebdd40687a99dcd0d35a37
Patoorlando2 <patoorlando2@gmail.com>
2024-10-14 18:07:46 +0000

    updating

--- Parcial1/puntoc.sh
+++ Parcial1/puntoc.sh
@@ -2,18 +2,6 @@
 
 #crear grupos secundarios
 
-# Ruta base
-ruta_base="/Examenes-UTN"
-
-# Crear la ruta base si no existe
-sudo mkdir -p "$ruta_base/alumno_1"
-sudo mkdir -p "$ruta_base/alumno_2"
-sudo mkdir -p "$ruta_base/alumno_3"
-sudo mkdir -p "$ruta_base/profesores"
-
-
-
-
 sudo groupadd grupoprogramadores
 sudo groupadd grupotester
 sudo groupadd grupoanalistas
@@ -37,14 +25,14 @@ sudo usermod -aG grupodisenadores disenadores
 sudo usermod -aG grupodisenadores vagrant
 
 
-sudo chown -R programadores:grupoprogramadores /Examenes-UTN/alumno_1
-sudo chmod -R 750 /ExamenesUTN/alumno_1
+sudo chown -R programadores:grupoprogramadores Examenes-UTN/alumno_1
+sudo chmod -R 750 Examenes-UTN/alumno_1
 
-sudo chown -R tester:grupotester /Examenes-UTN/alumno_2
-sudo chmod -R 760 /Examenes-UTN/alumno_2
+sudo chown -R tester:grupotester Examenes-UTN/alumno_2
+sudo chmod -R 760 Examenes-UTN/alumno_2
 
-sudo chown -R analistas:grupoanalistas /Examenes-UTN/alumno_3
-sudo chmod -R 700 /Examenes-UTN/alumno_3
+sudo chown -R analistas:grupoanalistas Examenes-UTN/alumno_3
+sudo chmod -R 700 Examenes-UTN/alumno_3
 
-sudo chown -R disenadores:grupodisenadores /Examenes-UTN/profesores
-sudo chmod -R 775 /Examenes-UTN/profesores
+sudo chown -R disenadores:grupodisenadores Examenes-UTN/profesores
+sudo chmod -R 775 Examenes-UTN/profesores
