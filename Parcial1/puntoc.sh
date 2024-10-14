0ba7633352a3abf1cf979e5c1b730142e7cdedce
Patoorlando2 <patoorlando2@gmail.com>
2024-10-14 03:46:31 +0000

    updating

--- Parcial1/puntoc.sh
+++ Parcial1/puntoc.sh
@@ -19,19 +19,14 @@ sudo adduser disenadores
 sudo usermod -aG grupodisenadores
 
 
-sudo chown -R programadores:grupoprogramadores ./Examenes-UTN/alumno_1
-sudo chmod -R 750 ./ExamenesUTN/alumno_1
+sudo chown -R programadores:grupoprogramadores /Examenes-UTN/alumno_1
+sudo chmod -R 750 /ExamenesUTN/alumno_1
 
-sudo chown -R tester:grupotester ./Examenes-UTN/alumno_2
-sudo chmod -R 760 ./Examenes-UTN/alumno_2
+sudo chown -R tester:grupotester /Examenes-UTN/alumno_2
+sudo chmod -R 760 /Examenes-UTN/alumno_2
 
-sudo chown -R analistas:grupoanalistas ./Examenes-UTN/alumno_3
-sudo chmod -R 700 ./Examenes-UTN/alumno_3
+sudo chown -R analistas:grupoanalistas /Examenes-UTN/alumno_3
+sudo chmod -R 700 /Examenes-UTN/alumno_3
 
-sudo chown -R disenadores:grupodisenadores ./Examenes-UTN/profesores
-sudo chmod -R 775 ./Examenes-UTN/profesores
-
-for dir in alumno_1 alumno_2 alumno_3; do
-	whoami > ~/parcial1/Examenes-UTN/$dir/validar.txt
-done
-
+sudo chown -R disenadores:grupodisenadores /Examenes-UTN/profesores
+sudo chmod -R 775 /Examenes-UTN/profesores
